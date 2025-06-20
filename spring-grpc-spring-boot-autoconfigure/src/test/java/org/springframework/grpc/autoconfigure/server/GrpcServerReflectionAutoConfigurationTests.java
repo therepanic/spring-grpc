a8229e47925f25992baad3c1dafae5381127656a
Andrey Litvitski <andrey1010102008@gmail.com>
2025-06-20 22:06:00 +0300

    polish `GrpcServerReflectionAutoConfiguration`
    Signed-off-by: Andrey Litvitski <andrey1010102008@gmail.com>

--- spring-grpc-spring-boot-autoconfigure/src/test/java/org/springframework/grpc/autoconfigure/server/GrpcServerReflectionAutoConfigurationTests.java
+++ spring-grpc-spring-boot-autoconfigure/src/test/java/org/springframework/grpc/autoconfigure/server/GrpcServerReflectionAutoConfigurationTests.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2023-2025 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.springframework.grpc.autoconfigure.server;
 
 import io.grpc.BindableService;
@@ -9,12 +24,19 @@ import org.springframework.grpc.server.lifecycle.GrpcServerLifecycle;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+/**
+ * Tests for {@link GrpcServerReflectionAutoConfiguration}.
+ *
+ * @author Haris Zujo
+ * @author Chris Bono
+ * @author Andrey Litvitski
+ */
 public class GrpcServerReflectionAutoConfigurationTests {
 
 	private ApplicationContextRunner contextRunner() {
 		return new ApplicationContextRunner()
 			.withConfiguration(AutoConfigurations.of(GrpcServerReflectionAutoConfiguration.class))
-			.withBean("noopServerLifecylcle", GrpcServerLifecycle.class, Mockito::mock)
+			.withBean("noopServerLifecycle", GrpcServerLifecycle.class, Mockito::mock)
 			.withBean(BindableService.class, Mockito::mock);
 	}
 
